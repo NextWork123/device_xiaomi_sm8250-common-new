c7dbf3c4f83f085230db4800e65e12867e52fa4c
Alexander Winkowski <dereference23@outlook.com>
2021-08-05 14:48:13 +0200

    raphael: Import Pixel thermal HAL
    * From hardware/google/pixel at 728fb99bbb910be05711421310efa6827aaaa4fa.
    
    Change-Id: I763b4dbef65084cfee337065b2c5ab465f69bca8

--- kona.mk
+++ kona.mk
@@ -468,6 +468,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Thermal HAL
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.pixel
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
